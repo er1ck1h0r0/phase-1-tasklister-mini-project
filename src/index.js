ad65c4f010f3b16feb879e37d56c08e93c7e35ed
eric <ericmaina11455@gmail.com>
2024-04-07 11:27:41 +0300

    assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDoInput(e.target.newTaskDescription.value)
+
+
+
+
+  })
+
+
 });
+
+
+
+function toDoInput(job) {
+  let li = document.createElement('li')
+  let del = document.createElement('button')
+  del.textContent = 'done'
+  del.setAttribute('class', 'delete')
+  del.classList.add('delete')
+  li.textContent = `${job}`
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+  li.appendChild(del)
+  del.addEventListener('click', () => del.parentNode.remove())
+
+
+
+}
+
